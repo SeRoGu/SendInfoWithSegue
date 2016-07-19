29b12a8648f155d28f825080a418f9f47c147cca
SeRoGu <seba@macbook-pro-de-seba.local>
2016-07-19 14:53:10 -0300

    Initial Commit

---
+++ SendInfoWithSegue/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SendInfoWithSegue
+//
+//  Created by Seba on 19/7/16.
+//  Copyright © 2016 Seba. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
